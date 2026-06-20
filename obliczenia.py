f5c67fd10e221a08d15f6674ac7877e3ff1265a6
__mikoolaaj__ <gn.mikolaj.astro@gmail.com>
2026-06-20 15:06:31 +0200

    Dodaje kalkulator

---
+++ obliczenia.py
@@ -0,0 +1,7 @@
+# Program: Czas podróży światła
+dystans_lata_swietlne = 4.2  # Proxima Centauri
+predkosc_swiatla = 1 
+
+czas = dystans_lata_swietlne / predkosc_swiatla
+
+print(f"Światło z tej gwiazdy leci do nas {czas} lat.")
