aa49c6650c1b7fbdbeeeeceb33dd2f1bfe8bd662
__mikoolaaj__ <gn.mikolaj.astro@gmail.com>
2026-06-21 13:17:35 +0200

    Dodaję if, elif i else

--- kalkulator.py
+++ kalkulator.py
@@ -1,12 +1,19 @@
 liczba_a = input("Podaj pierwszą liczbę: ")
 liczba_b = input("Podaj drugą liczbę: ")
+dzialanie = input("Wybierz działanie (+, -, *, /): ")
 liczba_a = int(liczba_a)
 liczba_b = int(liczba_b)
 wynik_dodawania = liczba_a + liczba_b
 wynik_odejmowania = liczba_a - liczba_b
 wynik_mnozenia = liczba_a * liczba_b
 wynik_dzielenia = liczba_a / liczba_b
-print("Wynik dodawania to:", wynik_dodawania)
-print("Wynik odejmowania to: ", wynik_odejmowania)
-print("Wynik mnożenia to: ", wynik_mnozenia)
-print("Wynik dzielenia to: ", wynik_dzielenia)
+if dzialanie == "+":
+    print("Wynik dodawania:", wynik_dodawania)
+elif dzialanie == "-":
+    print("Wynik odejmowania:", wynik_odejmowania)
+elif dzialanie == "*":
+    print("Wynik mnożenia:", wynik_mnozenia)
+elif dzialanie == "/":
+    print("Wynik dzielenia:", wynik_dzielenia)
+else:
+    print("Nieprawidłowe działanie")
