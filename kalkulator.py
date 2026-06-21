8c4411fbbed3476fdcba1c2ad8ede55835979c8c
__mikoolaaj__ <gn.mikolaj.astro@gmail.com>
2026-06-21 13:07:29 +0200

    Kalkulator 100%

---
+++ kalkulator.py
@@ -0,0 +1,12 @@
+liczba_a = input("Podaj pierwszą liczbę: ")
+liczba_b = input("Podaj drugą liczbę: ")
+liczba_a = int(liczba_a)
+liczba_b = int(liczba_b)
+wynik_dodawania = liczba_a + liczba_b
+wynik_odejmowania = liczba_a - liczba_b
+wynik_mnozenia = liczba_a * liczba_b
+wynik_dzielenia = liczba_a / liczba_b
+print("Wynik dodawania to:", wynik_dodawania)
+print("Wynik odejmowania to: ", wynik_odejmowania)
+print("Wynik mnożenia to: ", wynik_mnozenia)
+print("Wynik dzielenia to: ", wynik_dzielenia)
